39a0622f34d2efc1214779eac2150b64336fef8f
Hasnat Alam <hasnatalamofficial@gmail.com>
2025-06-14 11:31:51 +0530

    Delete ShopManagement directory

--- ShopManagement/src/main/java/com/example/onlineshop/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.example.onlineshop.service;
-
-import com.example.onlineshop.dto.UserDTO;
-import com.example.onlineshop.model.User;
-
-public interface AuthService {
-    User registerUser(UserDTO userDTO);
-}
